4963fd1286b194a2a1d3992bb3ad759484fc9c09
collins <kolichitutu110@gmail.com>
2022-09-07 15:20:05 -0700

    payment displays

--- Client/src/Components/Search.js
+++ Client/src/Components/Search.js
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
 
-import '../Styles/Searchbar.css'
+import "../Styles/Searchbar.css";
 import Data from "../API/Api";
-import './ApiData'
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
-import 'animate.css';
+import "./ApiData";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 
+import Airtel from '../images/airtel.png'
+import Mtn from '../images/mtn.png'
+import Zamtel from '../images/zamtel.png'
+import Visa from '../images/visa.webp'
+import MC from '../images/mastercard.png'
 
-//payment gatddseway 
+//payment gatddseway
 // import FWH from '../Components/PaymentModal'
 // import  { FlutterWaveButton, useFlutterwave }  from "flutterwave-react-v3";
 
-
-
 const Search = () => {
-const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("");
 
-const searchText = (event) => {
+  const searchText = (event) => {
     setFilter(event.target.value);
   };
-  let dataSearch = Data.cardData.filter(item =>{
-    return Object.keys(item).some(key =>
-      item[key].toString().toLowerCase().includes(filter.toString().toLowerCase()))
+  let dataSearch = Data.cardData.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filter.toString().toLowerCase())
+    );
   });
-
 
   return (
     <section className="py-2 container">
@@ -42,65 +47,76 @@ const searchText = (event) => {
           </div>
         </div>
 
-     
-     
-{/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button> */}
+        {/* MODAL */}
 
-<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        ...
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Mooto Food Payment Options
+                </h5>
+                <button
+                  type="button"
+                  className=" btn btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                      <p> We Accept</p>
+              <img src={Airtel} alt=""/> 
+              <img src={Mtn} alt=""/> 
+              <img src={Zamtel} alt=""/> 
+              <img src={MC} alt=""/> 
+              <img src={Visa} alt=""/> 
 
-    
-          
+
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn">
+                  Procced 
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {dataSearch.map((item, index) => {
-          return(
- 
-          //   <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4 animate__pulse"  key={index}>
-          //   <div className="card p-o overflow-hidden h-100  " key={index}>
-          //     <img src={item.img} className="card-img-top"  alt="" />
-          //     <div className="card-body"  key={index}>
-          //       <h5 className="card-title"> {item.title}</h5>
-          //       <p className="card-text"> {item.desc} </p>
-          //     </div>
-          //   </div>
-          // </div>
-<div className="card "  key={index}>
-  <img src={item.img} className="card-img-top" alt="..." />
-  <div className="card-body" key={index}>
-    <h5 className="card-title">{item.title}</h5>
-    <p className="card-text">{item.Price}</p>
+          return (
+            <div className="card " key={index}>
+              <img src={item.img} className="card-img-top" alt="..." />
+              <div className="card-body" key={index}>
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.Price}</p>
 
-  <button type="button" className="btn sbtn" data-bs-toggle="modal" data-bs-target="#exampleModal" >Buy</button>
-
-
-
-
-  </div>
-</div>
-
-          )
-        
-      })}
- 
+                <button
+                  type="button"
+                  className="btn sbtn"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
-    
   );
 };
 export default Search;
